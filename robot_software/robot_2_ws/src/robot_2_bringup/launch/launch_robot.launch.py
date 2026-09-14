2c3b457e2f8a6163677498cf34e9480a30aad354
Dickson <kabirud080@gmail.com>
2026-09-14 10:11:32 +0300

    RDI-183: Add additional debugging commands to launch file for hardware components

--- robot_software/robot_2_ws/src/robot_2_bringup/launch/launch_robot.launch.py
+++ robot_software/robot_2_ws/src/robot_2_bringup/launch/launch_robot.launch.py
@@ -121,3 +121,5 @@ def generate_launch_description():
     #ls -l /dev/ttyUSB_ARDUINO
     #ros2 control list_controllers -c /robot_2/controller_manager
     #ros2 control list_hardware_components -c /robot_2/controller_manager
+    #vcgencmd get_throttled
+    #vcgencmd pmic_read_adc
